75cc41b93558276801b8e3b3780284f8eb3bf44a
Dumota02 <edumota02@gmail.com>
2021-05-05 21:01:00 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello world!</h1>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <h1 className="text-primary">Hello world!</h1>
+        <DataTable></DataTable>
+      </div>
+      <Footer></Footer>
+    </>
+    //fragment <> avisando pro react que tudo é  uma coisa só pois a função só retorna 1 coisa
+    //shift + alt +f formata a tela
   );
 }
 
